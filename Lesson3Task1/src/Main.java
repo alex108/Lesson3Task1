b529d316950ad4eadc788d862df548b0ae3f2703
alex.108 <msshanti2017@gmail.com>
2024-03-07 14:53:22 +0300

    задание 1 урок 3

--- Lesson3Task1/src/Main.java
+++ Lesson3Task1/src/Main.java
@@ -3,20 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        // 1.Get username
+        // 1. Get username
         System.out.println("Hello, please enter your name.");
         Scanner scanner = new Scanner(System.in);
         String userName = scanner.nextLine();
 
-        // 2.Get user's surname
+        // 2. Get user's surname
         System.out.println("Please enter your surname.");
         String userSurname = scanner.nextLine();
 
-        // 3.Get user's profession
+        // 3. Get user's profession
         System.out.println("Please enter your profession.");
         String userProfession = scanner.nextLine();
 
-        // 4.Display user's name, surname, profession
+        // 4. Display user's name, surname, profession
         System.out.format("Your name is %s, your surname is %s, your profession is %s.", userName, userSurname, userProfession);
 
     }
